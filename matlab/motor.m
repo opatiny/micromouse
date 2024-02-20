3f7e8cc74d20e2d2d541cda7f5858d7812af0cd1
Oceane Patiny <oceane@patiny.com>
2024-02-20 16:23:40 +0100

    matlab: enhance motors script

--- matlab/motor.m
+++ matlab/motor.m
@@ -1,6 +1,9 @@
 %% Motors
 clear; clc; close all;
 
+%% Generic variables
+g = 9.81; % kg*m/s^2
+
 %% Maximum motor speed
 % max desired speed and accel
 vmax = 2; % m/s -> typically 1.5-2 m/s
@@ -21,7 +24,7 @@ n = vmax/p*60 % rpm
 omega = vmax/r
 
 % max acceleration -> the acceleration that we'd like to reach
-amax = 10; % m/s^2, wiki: max accel up to 2g
+amax = 1.5*9.81; % m/s^2, wiki: max accel up to 2g
 alpha = amax/r
 
 %% Total inertia relative to the motors
@@ -31,15 +34,33 @@ Jmot = 1e-7; % kgm^2 -> rotor inertia, approx 1gcm^2 (estimation random maxon mo
 Jred = 0; % no gearbox
 i = 1; % - , gearbox reduction
 eta = 1; % gearbox efficiency
-eta_floor = 0.8; % - , floor efficiency
+eta_floor = 1; % - , floor efficiency -> we neglect the rolling resistance
 
 % wheel inertia estimation
 m_wheel = 0.025; % kg
-Jwheel = 1/2 * m_wheel * r^2;
+Jwheel = 1/2 * m_wheel * r^2; % approximate as full cylinder
 
-Jrtot = Jmot + Jred + 1/(eta*i^2)*(Jwheel + M*r^2/(2*eta_floor))
+Jrtot = Jmot + Jred + 1/(eta*i^2)*(Jwheel + M*r^2/(2*eta_floor));
 
 %% Maximum torque
 M_mot = Jrtot * alpha
 
+%% Maximal mechanical power
+Pmec = omega * M_mot
+% Pel = U*I 
+
 %% Maximal acceleration depending on friction coefficient
+% if the acceleration is bigger the wheels will slip
+mu0 = 0.8; % -
+N = 4; % -, total number of wheels
+M_mot_max = r*mu0*M*g/N
+alpha_max = M_mot_max/(eta*i*Jrtot); % rad/s^2
+a_max = alpha_max*r % m/s^2
+
+Pmax_fr = omega*M_mot_max
+
+% distance to reach max speed
+deltax = 0.5; % m
+vf = sqrt(2*a_max*deltax)
+
+vlim = 145*2*pi*r/60
