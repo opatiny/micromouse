d35f93d0358f77fd077923317fb50e988973f61a
Oceane Patiny <oceane@patiny.com>
2024-06-21 17:19:58 +0200

    matlab: test low pass filter for PID derivative

---
+++ matlab/dynamics.m
@@ -0,0 +1,24 @@
+%% Robot dynamics
+% compute max robot linear and angular speeds
+
+clear; clc; close all;
+
+%% variables
+d = 0.032; % m , wheel diameter
+maxWheelSpeed = 550; % rpm
+wheelBase = 0.1; % m
+
+%% max linear speed
+r = d/2;
+p = 2*r*pi; % perimeter
+
+v_max = p*maxWheelSpeed/60 % m/s
+
+%% max angular speed of the robot
+r = wheelBase/2;
+
+omega_max = v_max/r % rad/sec
+
+omega_max_degSec = omega_max*180/pi
+
+n_max = omega_max * 30/pi % rpm
---
+++ matlab/lowPassFilter.m
@@ -0,0 +1,48 @@
+%% Test low pass filter
+clc; clear; clf;
+
+% filtering will help a lot with the derivative term in PID controllers
+
+%% load data
+fileName  = 'controllerDelayImpact1ms.csv';
+data = readtable(['./data/' fileName]);
+time = (data.time - data.time(1))/1000; % time in seconds
+speed = data.leftSpeed;
+
+%% low-pass filter
+filt005 = filter(speed, 0.05);
+filt01 = filter(speed, 0.1);
+filt02 = filter(speed, 0.2);
+
+% how to estimate how good the filtering is?
+varSpeed = var(speed)
+var005 = var(filt005)
+var01 = var(filt01)
+var02 = var(filt02)
+
+
+%% plot
+plot(time, speed,'.-');
+hold on;
+plot(time, filt02, '.-');
+plot(time, filt01, '.-');
+plot(time, filt005, '.-');
+
+
+hold off;
+grid on;
+xlabel('Time [s]');
+ylabel('Speed [rpm]');
+legend('original', '\alpha = 0.2', '\alpha = 0.1', '\alpha = 0.05');
+xlim([0 1.5]);
+
+
+function filtered = filter(speed, alpha)
+m = length(speed);
+sensor = 0;
+filtered = zeros(1,m);
+for i=1:m
+sensor = alpha * speed(i) + (1-alpha) * sensor;
+filtered(i) = sensor;
+end
+end
